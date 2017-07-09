d7ba950a7e43faa60811cd9484aaece640f29008
maciej <m.p.dutkiewicz@gmail.com>
2017-07-09 15:09:05 +0200

    pierwsze oddanie

---
+++ scripts.js
@@ -0,0 +1,17 @@
+function getTriangleArea(a, h) {
+  if (a<=0 && h<=0) {
+      consol.log('Nieprawidłowe dane');
+      }
+      else if (a<=0 || h<=0) {
+        consol.log('Nieprawidłowe dane');
+      }
+      else 
+        return a*h/2;
+}
+console.log(getTriangleArea(10, 6));
+var triangle1Area = getTriangleArea(10, 15);
+console.log(triangle1Area);
+var triangle2Area = getTriangleArea(10, 14);
+console.log(triangle2Area);
+var triangle3Area = getTriangleArea(10, 13);
+console.log(triangle3Area);
