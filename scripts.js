9a4ba362e3d3dd95394a046471d7a7a8001cc80c
maciej <m.p.dutkiewicz@gmail.com>
2017-07-17 13:53:53 +0200

    poprawiono/skasowano jeden warunek

--- scripts.js
+++ scripts.js
@@ -1,12 +1,10 @@
 function getTriangleArea(a, h) {
-  if (a<=0 && h<=0) {
-      consol.log('Nieprawidłowe dane');
-      }
-      else if (a<=0 || h<=0) {
+      if (a<=0 || h<=0) {
         consol.log('Nieprawidłowe dane');
       }
-      else 
+      else {
         return a*h/2;
+    }
 }
 console.log(getTriangleArea(10, 6));
 var triangle1Area = getTriangleArea(10, 15);
